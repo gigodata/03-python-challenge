ee55a56c33e65c38378e8e7fb9f092f50abd592f
Adrian Santos <adrian.santos@gigodata.com>
2023-09-27 14:36:02 -0400

    CSV file with budget data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,7 @@
 # main.py
-# Used for PyBank analysis
+
+# Script for PyBank analysis
+# Result returned is a summary analysis of MOM changes in P&L over 7+ yearly period.
+# Created: 2023-09-27
+# Author: Adrian Santos
+# v1.0
